2ba8a0a73383c90f27239ca7c42ddcf9151ce868
Hilgard van Rooyen <hilgardvr@gmail.com>
2020-04-01 07:47:02 +0200

    wip calcTimeSpend

--- src/View/DisplayTime.elm
+++ src/View/DisplayTime.elm
@@ -22,3 +22,10 @@ stringTime time zone =
         second  = padTime (String.fromInt (Time.toSecond zone time))
     in
         (hour ++ ":" ++ minute ++ ":" ++ second)
+
+calcTimeSpendFrom: Time.Posix -> Time.Posix -> String
+calcTimeSpendFrom: startTime endTime =
+    let
+        totalMs = (Time.posixToMillis endTime) - (Time.posixToMillis startTime)
+        hours   = totalMs // 3600000
+        minutes = (total - hours)
