e75cfcec8aa55a481a40d506effb3a32b5b48135
Hilgard van Rooyen <hilgardvr@gmail.com>
2020-04-08 07:51:26 +0200

    wip time to posix buggy

--- src/View/FilterShowCompleted.elm
+++ src/View/FilterShowCompleted.elm
@@ -6,7 +6,13 @@ import Model.Model exposing (..)
 
 filterShowCompleted: Model -> List Completed
 filterShowCompleted model =
-    model.completedList
+    let
+        from = Time.posixToMillis (getShowDateTimeFromString model.showCompletedFromDate model.showCompletedFromTime)
+        to = Time.posixToMillis (getShowDateTimeFromString model.showCompletedToDate model.showCompletedToTime)
+    in
+        List.filter 
+            (\completedItem -> (Time.posixToMillis completedItem.startTime > from) && (Time.posixToMillis completedItem.startTime < to))
+            model.completedList
 
 
 getListElementByIndex: List a -> Int -> Maybe a
@@ -15,19 +21,32 @@ getListElementByIndex lst index =
         [] -> Nothing
         h::t ->
             if index < 1
-            then List.head lst
+            then Just h
             else getListElementByIndex t (index - 1)
+
+
+getIntFromStringList: (List String -> Int -> Maybe String) -> List String -> Int -> Int
+getIntFromStringList f lst index =
+    case f lst index of
+        Just y -> Maybe.withDefault 0 (String.toInt y)
+        Nothing -> 0
+
 
 getShowDateTimeFromString: String -> String -> Time.Posix
 getShowDateTimeFromString dateString timeString =
     let
-        dates = String.split ("-") dateString
+        dates = String.split "-" dateString
+        times = String.split ":" timeString
 
-        secs = 1000
-        mins = secs * 60
+        mins = 1000 * 60
         hours = mins * 60
         days = hours * 24
         months = days * 30
         years = days * 365
+        year = (getIntFromStringList getListElementByIndex dates 0 - 1970) * years
+        month = (getIntFromStringList getListElementByIndex dates 1 - 1) * months
+        day = (getIntFromStringList getListElementByIndex dates 2 - 1) * days
+        hour = getIntFromStringList getListElementByIndex times 0 * hours
+        minute = getIntFromStringList getListElementByIndex times 1 * mins
     in
-        Time.millisToPosix 0
+        Time.millisToPosix (year + month + day + hour + minute)
