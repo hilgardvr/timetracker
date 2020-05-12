cd90a3750caa224b47637c6b9469b42b1733f74f
Hilgard van Rooyen <hilgardvr@gmail.com>
2020-05-12 07:45:46 +0200

    wip elm-ui implement

--- src/View/LoginView.elm
+++ src/View/LoginView.elm
@@ -1,41 +1,54 @@
 module View.LoginView exposing (loginView)
 
-import Html exposing (..)
+import Html exposing (Html, input)
 import Model.Model exposing (..)
 import Html.Events exposing (onInput, onClick)
--- import Html.Attributes exposing (type_, placeholder, value, selected, checked)
-import Html.Attributes exposing (..)
+import Html.Attributes exposing (type_, placeholder, value)
+import Element exposing (Element, width, centerY, el, row, fill, alignRight, spacing, text, rgb255)
+import Element.Input exposing (button)
+import Element.Background as Background
 
 
-loginView: Model -> Html Msg
+loginView: Model -> Element Msg
 loginView model =
     case model.loginStatus of
         LoggedIn -> viewLoggedIn
         Pending -> viewPending
         LoggedOut -> viewLoggedOut model
 
-viewLoggedIn: Html Msg
+viewLoggedIn: Element Msg
 viewLoggedIn = 
-    div []
+    row []
         [ text "You are logged in"
         , button
-            [ onClick Logout ]
-            [ text "Logout"]
+            [ Background.color (rgb255 240 0 245)
+            , Element.focused [ Background.color (rgb255 111 111 111) ]
+            ]
+            { onPress = Just Logout
+            , label = text "Logout"
+            } 
+            -- [ onClick Logout ]
+            -- [ text "Logout"]
         ]
 
-viewPending: Html Msg
+viewPending: Element Msg
 viewPending =
-    div []
+    row []
         [ text "We are processing your login request"
         ]
 
-viewLoggedOut: Model -> Html Msg
+viewNavBar =
+    row [ width fill, centerY, spacing 30 ]
+        [ text "Time-me"
+        ]
+
+viewLoggedOut: Model -> Element Msg
 viewLoggedOut model =
-    div []
+    row []
         [ text "You are not logged in - your all changes will be lost when you leave the site"
-        , br [] [] 
+        -- , br [] [] 
         , text "Create a free account or log in"
-        , br [] [] 
+        -- , br [] [] 
         , input [ type_ "text", placeholder "email", value model.userName, onInput ChangeUserName ] []
         , input [ type_ "password", placeholder "password", value model.password, onInput ChangePassword ] []
         , button  
